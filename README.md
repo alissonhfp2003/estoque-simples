8cc353b5005c397735f08132b085d22dc5c30659
alissonhfp2003 <alissonhfp2003@gmail.com>
2024-11-25 21:29:30 -0300

    commit aprovado

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # estoque-simples
 estoque-simples
+
+para compilar é preciso usar algum compilador em C
+meu caso usei um online qualquer
+https://www.onlinegdb.com/online_c_compiler
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 
@@ -10,6 +11,33 @@ typedef struct {
 } Produto;
 Produto estoque[MAX_PRODUTOS];
 int totalProdutos = 0;
+
+void adicionarProduto() {
+    if (totalProdutos >= MAX_PRODUTOS) {
+        printf("Estoque cheio!\n");
+        return;
+    }
+    
+    Produto p;
+    
+    printf("Digite o nome do produto: ");
+    scanf("%s", p.nome);
+    printf("Digite a quantidade: ");
+    scanf("%d", &p.quantidade);
+    printf("Digite o preço: ");
+    scanf("%f", &p.preco);
+    
+    estoque[totalProdutos] = p;
+    totalProdutos++;
+    printf("Produto adicionado com sucesso!\n");
+}
+
+void consultarEstoque() {
+    printf("Estoque:\n");
+    for (int i = 0; i < totalProdutos; i++) {
+        printf("Produto: %s | Quantidade: %d | Preço: %.2f\n", estoque[i].nome, estoque[i].quantidade, estoque[i].preco);
+    }
+}
 
 void venderProduto() {
     char nome[50];
@@ -33,6 +61,39 @@ void venderProduto() {
         }
     }
     printf("Produto não encontrado!\n");
+}
+
+int main() {
+    int opcao;
+    
+    do {
+        printf("\nMenu Principal:\n");
+        printf("1. Adicionar produto\n");
+        printf("2. Consultar estoque\n");
+        printf("3. Vender produto\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        
+        switch (opcao) {
+            case 1:
+                adicionarProduto();
+                break;
+            case 2:
+                consultarEstoque();
+                break;
+            case 3:
+                venderProduto();
+                break;
+            case 4:
+                printf("Saindo do programa...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 4);
+    
+    return 0;
 }
 
 void consultarEstoque() {
